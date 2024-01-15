feae512f8ef9b3e8d29577a58f84f474929c0265
nabilhusniros <nabilhusnir@gmail.com>
2024-01-15 15:04:10 +0800

    Initial commit

---
+++ 04-functions.js
@@ -0,0 +1,23 @@
+// Function to calculate the sum of two numbers
+function sum(a, b) {
+    // Return the sum of a and b
+    return a + b;
+}
+
+// Log the sum of 10 and 10 to the console
+console.log(sum(10, 10)) // 20
+
+// Function to calculate the product of two numbers
+function product(a, b) {
+    // Return the product of a and b
+    return a * b;
+}
+
+// Log the product of 5 and 5 to the console
+console.log(product(5, 5))
+
+// Calculate the product of two large numbers and assign it to a variable
+const bigProduct = product(1234, 1234);
+
+// Log the value of the big product to the console
+console.log(bigProduct)
